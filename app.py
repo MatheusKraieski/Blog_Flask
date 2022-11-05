9dbf3e38d2dbd237cd358c1b39f250bca1f3dfd7
Matheus Kraieski <89086545+MatheusKraieski@users.noreply.github.com>
2022-11-05 16:24:37 -0300

    Chore: keeping the adjustments in the registerform and loginform

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'bcc9bdc83147456ebd8da8bfe5eec301'
 
@@ -23,19 +23,26 @@ posts = [
 
 ]
 
+
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
+
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
 
-@app.route("/register")
+
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    return render_template('register.html', title='register', form=form)
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
 
 @app.route("/login")
 def login():
