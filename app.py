b82f787cc3efd00a27e8a7c09ba6ab255c2db819
Matheus <89086545+MatheusKraieski@users.noreply.github.com>
2022-11-08 10:54:15 -0300

    chore: finish login and register page

--- app.py
+++ app.py
@@ -44,9 +44,15 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@login.com' and form.password.data == 'password':
+            flash('you have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+             flash('Login Unsuccessful, please check', 'danger')   
     return render_template('login.html', title='login', form=form)
 
 
